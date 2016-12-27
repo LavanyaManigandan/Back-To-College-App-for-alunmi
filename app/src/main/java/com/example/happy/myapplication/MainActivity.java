ec6e25fba05ea3333355d472c6c63fa5086fae4e
tamilarasan <tamilarasanvr@gmail.com>
2016-12-27 15:59:18 +0530

    27 dec 2016 3:58pm

--- app/src/main/java/com/example/happy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/happy/myapplication/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.happy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello");
+        name=(TextView)findViewById(R.id.textView2);
+        name.setText(Html.fromHtml(getResources().getString(R.string.create)));
+
     }
 }
